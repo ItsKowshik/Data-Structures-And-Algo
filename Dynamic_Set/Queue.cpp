189aae996c7f5e8f25138e8ab043aee69a996b2b
Kowshik Arko Dey <kowshikisme@gmail.com>
2023-05-25 22:12:50 +0600

    Added comments

--- Dynamic_Set/Queue.cpp
+++ Dynamic_Set/Queue.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 class Queue{
     private:
-    int head;
-    int tail;
-    int size;
-    int q[100];
+    int head;               //Pointer to the head of the queue
+    int tail;               //Pointer to the tail of the queue
+    int size;               //Store the size of the queue
+    int q[100];             //The queue implemented as a dynamic array
     public:
+    //Constructor to create a queue of a given size
     Queue(int n){
         cout<<"Queue created successfully"<<endl;
         head = tail = 0;
         size = n;
     }
+    //Subtroutine to check if the queue is empty
     bool empty(){
         if(head == tail){
             return true;
@@ -22,42 +24,45 @@ class Queue{
             return false;
         }
     }
+    //Subroutine to check if the queue is full
     bool full(){
-        if((tail+1==head)||((head == 0) &&(tail == size))){
+        if((tail+1==head)||((head == 0) &&(tail == size))){  //The boundary conditions
             return true;
         }
         else{
             return false;
         }
     }
+    //Add a value to the end of the queue
     void enqueue(int k){
         if(full()){
-            cout<<"Queue is full"<<endl;
+            cout<<"Queue is full"<<endl;        //The queu is full
         }
         else{
-            if(tail == size){
+            if(tail == size){                   //The circular condition to add the value to the beginning of the queue
                 tail = 0;
                 q[tail]=k;
             }
-            else{
+            else{                               //Add an element to the end of the queue
                 q[tail]=k;
                 tail = tail + 1;
             }
             cout<<"Successfully enqueued"<<endl;
         }
     }
+    //Subroutine to remove the front element from the queue
     void dequeue(){
         if(empty()){
-            cout<<"Queue is empty"<<endl;
+            cout<<"Queue is empty"<<endl;       //Empyty Queue
         }
         else{
             cout<<"Successfully dequeued "<<q[head]<<endl;
             q[head]=0;
-            if(head == size){
+            if(head == size){                   //Dequeue last element
                 head = 0;
             }
             else{
-                head = head + 1;
+                head = head + 1;                //Shift head pointer
             }
         }
     }
